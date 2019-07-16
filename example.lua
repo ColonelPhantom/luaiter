8271e49eee5f03493b9530083a6245f62e5853c3
Quinten Kock <quintenkock@gmail.com>
2019-07-16 21:56:06 +0200

    Implement factorial in example

--- example.lua
+++ example.lua
@@ -12,11 +12,10 @@ function printret(...)
     return ...
 end
 
-function counter(i, max)
+function counter(i)
     i = i - 1
     return function()
         i = i + 1
-        if max and i > max then return end
         return i
     end
 end
@@ -29,12 +28,20 @@ function accumulator(start)
     end
 end
 
-local iterator1 = iterator.fromfn(counter(-1,3))
-local iterator2 = iterator.fromfn(counter(5, 8))
-print(iterator1:chain(iterator2)
-    -- :map(printret)
-    :foreach(accumulator()))
+-- local iterator1 = iterator.fromfn(counter(-1)):take(2)
+-- iterator1:foreach(print)
 
+function factorial(n)
+    local i = 0
+    local counter = function()
+        i = i + 1
+        return i
+    end
+    local result = 1
+    return iterator.fromfn(function() i=i+1 return i end):take(n)
+        :foreach(function(v) result = v * result return result end)
+end
+print(factorial(5))
 
 -- local itercount = iterator.fromfn(counter(5)):filter(isEven)
 --         :zip(iterator.fromfn(counter(-30)))
